ad616ec3ac706dc5fd65fe83a9a4408b69a3e31e
3lawix9 <92171974+3lawix9@users.noreply.github.com>
2023-06-14 11:32:12 +0300

    images is there

--- src/Compo/Cards.js
+++ src/Compo/Cards.js
@@ -1,50 +1,57 @@
-import React from 'react'
-import Carditem from './Carditem'
-import './Cards.css'
-import './../App.css'
+import React from 'react';
+import Carditem from './Carditem';
+import './Cards.css';
+import './../App.css';
+import img9 from '../images/img-9.jpg';
+import img2 from '../images/img-2.jpg';
+import img3 from '../images/img-3.jpg';
+import img4 from '../images/img-4.jpg';
+import img8 from '../images/img-8.jpg';
 
 function Cards() {
   return (
     <div className='cards'>
-        <h1>Check out these EPIC View!</h1>
-        <div className='cards__container'>
-            <div className="cards-wrapper">
-                <ul className="cards__items">
-                    <Carditem src='.\..\images\img-9.jpg' 
-                    text='You can see the Waterfall and enjoy it , Dont forget to contant us'
-                    label='Adventure'
-                    path='/services'/>
-                    <Carditem
-                    src='.\..\images\img-2.jpg'
-                    text='Travel through the Islands of Bali in a Private Cruise'
-                    label='Luxury'
-                    path='/services'
+      <h1>Check out these EPIC Views!</h1>
+      <div className='cards__container'>
+        <div className="cards-wrapper">
+          <ul className="cards__items">
+            <Carditem
+              src={img9}
+              text='You can see the Waterfall and enjoy it. Dont forget to contact us'
+              label='Adventure'
+              path='/services'
             />
-                </ul>
-                <ul className='cards__items'>
-                    <Carditem
-                    src='my-app\src\images\img-3.jpg'
-                    text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
-                    label='Mystery'
-                    path='/services'
-                    />
-                    <Carditem
-                    src='my-app\src\images\img-4.jpg'
-                    text='Experience Football on Top of the Himilayan Mountains'
-                    label='Adventure'
-                    path='/products'
-                    />
-                    <Carditem
-                    src='my-app\src\images/img-8.jpg'
-                    text='Ride through the Sahara Desert on a guided camel tour'
-                    label='Adrenaline'
-                    path='/sign-up'
-                    />
-                </ul>
-            </div>
+            <Carditem
+              src={img2}
+              text='Travel through the Islands of Bali in a Private Cruise'
+              label='Luxury'
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <Carditem
+              src={img3}
+              text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+              label='Mystery'
+              path='/services'
+            />
+            <Carditem
+              src={img4}
+              text='Experience Football on Top of the Himalayan Mountains'
+              label='Adventure'
+              path='/products'
+            />
+            <Carditem
+              src={img8}
+              text='Ride through the Sahara Desert on a guided camel tour'
+              label='Adrenaline'
+              path='/sign-up'
+            />
+          </ul>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Cards
+export default Cards;
