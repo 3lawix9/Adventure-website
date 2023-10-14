0d157e37a77f44a7301aa74d10f348f7064057c2
3lawix9 <92171974+3lawix9@users.noreply.github.com>
2023-10-14 11:30:38 +0300

    Fix deploy issue

---
+++ build/precache-manifest.b704e166715915cfa742bf63f0e8b981.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "64eb7faefcac9c0b07ea",
+    "url": "/static/css/main.8cdf94ec.chunk.css"
+  },
+  {
+    "revision": "64eb7faefcac9c0b07ea",
+    "url": "/static/js/main.4248a47a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e58029405f5dc41df13f",
+    "url": "/static/js/2.ecf96575.chunk.js"
+  },
+  {
+    "revision": "d211c298f5dddd6d141ac9ebee92d8a1",
+    "url": "/static/media/img-8.d211c298.jpg"
+  },
+  {
+    "revision": "bd11375f25b4d7b599e2065c9e916507",
+    "url": "/static/media/img-6.bd11375f.jpg"
+  },
+  {
+    "revision": "cd1d1c978d0030f14f1d8a7690eb8398",
+    "url": "/static/media/img-1.cd1d1c97.jpg"
+  },
+  {
+    "revision": "3cb2fa2c0a0b936a1d23bb3bf139a120",
+    "url": "/static/media/video-2.3cb2fa2c.mp4"
+  },
+  {
+    "revision": "523a89f64e53deb7aa02e9ff0fa6d18f",
+    "url": "/static/media/img-9.523a89f6.jpg"
+  },
+  {
+    "revision": "4143d240af520e4a8b565060c6e6277b",
+    "url": "/static/media/img-2.4143d240.jpg"
+  },
+  {
+    "revision": "65b53910f670be0b3337cb65bad3d7f6",
+    "url": "/static/media/img-3.65b53910.jpg"
+  },
+  {
+    "revision": "0ec923fc0fe65b0349ffbf93b33fd7c6",
+    "url": "/static/media/img-4.0ec923fc.jpg"
+  },
+  {
+    "revision": "21e3b5cae00c10e2e79cfe3eb35cb943",
+    "url": "/index.html"
+  }
+];
--- src/Compo/Herosection.js
+++ src/Compo/Herosection.js
@@ -2,32 +2,33 @@ import React from 'react';
 import { Button } from './Button';
 import './Herosection.css';
 import '../App.css';
+import video2 from '../videos/video-2.mp4'
 
 
 
 function Herosection() {
   return (
     <div className='hero-container'>
-        {/* <video  autoPlay loop muted  >
-        <source src={video2} type="video/webm" />
-        </video> */}
-        <h1>ADVENTURE AWAITS</h1>
-
-        <p>What are you waiting for?</p>
-        <div className='hero-btns'>
-            <Button className= 'btns'
-            buttonStyle= 'btn--outline'
-            buttonSize= 'btn--large' >
-                GET STARTED
-            </Button>
-
-            <Button className= 'btns'
-            buttonStyle= 'btn--primary'
-            buttonSize= 'btn--large' >
-                WATCH TRAILER <i className="fas fa-play-circle"/>
-            </Button>
-        </div>
-        
+      <video src={video2} autoPlay loop muted />
+      <h1>ADVENTURE AWAITS</h1>
+      <p>What are you waiting for?</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('hey')}
+        >
+          WATCH TRAILER <i className='far fa-play-circle' />
+        </Button>
+      </div>
     </div>
   )
 }
